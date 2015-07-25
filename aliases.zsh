052aaa8b5dcc58b4f9e72e4b0b65b3c15d2a2751
Danilo Barboza <danilo.barboza@gmail.com>
2015-07-24 18:39:57 -0700

    Add aliases file with my most used aliases from yadr

---
+++ aliases.zsh
@@ -0,0 +1,13 @@
+alias ccat='pygmentize -g'
+alias gdw='./gradlew'
+
+#git
+alias gsr='git svn rebase'
+alias gsd='git svn dcommit'
+alias gst='git stash'
+alias gsp='git stash pop'
+alias gpl='git pull'
+alias gplr='git pull --rebase'
+alias gps='git push'
+alias ga='git add -A'
+alias gcm='git commit -m'
