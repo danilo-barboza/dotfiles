c52470f824965f50c14a4cdc852bf5a2811ea331
Danilo Barboza <277004+danilo-barboza@users.noreply.github.com>
2021-04-14 18:07:39 +0200

    Update brew installation process and make install-apps.sh executable 💅🏽

--- install-apps.sh
+++ install-apps.sh
@@ -6,7 +6,7 @@ set -o errexit
 
 if test ! $(which brew); then
   echo "Installing homebrew..."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 while read -r line; do
